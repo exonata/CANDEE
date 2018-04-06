fc6beb895ace5c18c3970183c3a671fcc7ec7fa1
exonata <renata.smith@live.com>
2018-04-06 15:46:01 -0700

    reduce function size

--- main.py
+++ main.py
@@ -6,16 +6,14 @@ import RPi.GPIO as GPIO
 import csv
 import logging
 
-
-
 # global commands
 def global_define():
-    #cmd out settings
+    # cmd out settings
     global cmdDATA, cmdDOUT, cmdAOUT
     cmdDATA = "DATA\r"
     cmdDOUT = "DOUT\r"
     cmdAOUT = "AOUT\r"
-    #DOUT commands
+    # DOUT commands
     global RELAY_0, RELAY_1, RELAY_2, RELAY_3, OFF, ON, TOGGLE
     RELAY_0 = 0  # Add comments/change dout name to match what pin is controlling
     RELAY_1 = 1
@@ -35,40 +33,34 @@ filelocation = "/logs/"
 filename = filelocation + "testfile_" + str(time.time()) + ".csv"
 fileheader = "Time, DIN1 , DIN2, DIN3, DIN4,DIN5,DIN6,DIN7,DIN8,DOUT0,DOUT1,DOUT2,DOUT3, AIN0, AIN0,AIN0,AIN0,AOUT0,AOUT0,AOUT0,AOUT0"
 
-dataCounter = 0
-
-# Global data lists
-
 
 # pin definitions
 buttonPin = 4
 
 # pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button pin set as input w/ pullup
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button pin set as input w/ pullup resistor
 
 #main function
-def main(port, baud, filename):
-    ser = init_port(port, baud)
-    dataThread = threading.Thread(target=serialHandler, args=(ser, "DATA\r"))
-    dataThread.start()
-    serialHandler()
-    print("test in progress...")
-    time.sleep(5)
+#def main(port, baud, filename):
 
 
 # setup serial port
 def init_port(port, baud):
     return serial.Serial(port, baud, timeout=1)
 
-
+# initiate file and add header
 def init_logFile(filename, fileheader):
     with open('filename', 'wb') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(fileheader)
 
+#button press
+def button_press(channel):
+    print("button pressed on pin 4")
 
-# file writer
+
+# file writer for data out
 def fileWriter(time, dinputs, douputs, ainput, aoutputs, filename):
     #dataWrite = []
     rows = len(time)
@@ -87,63 +79,74 @@ def fileWriter(time, dinputs, douputs, ainput, aoutputs, filename):
     #     # csv.writer(csv_file).writerow([for i in range time:])
 
 
-# raw data handleer
-# def handle_data(raw_data, tag, dataCounter):
-#     print(raw_data)
-#     data = raw_data.split('|')
-#     TIME[:][dataCounter] = data[0].split("\t")
-#     DINPUTS[:][dataCounter] = data[1].split("\t")
-#     DOUTPUTS[:][dataCounter] = data[2].split("\t")
-#     AINPUTS[:][dataCounter] = data[3].split("\t")
-#     AOUTPUTS[:][dataCounter] = data[4].split("\t")
-#     dataCounter += 1
-
-    # set up thread generators
-def serialHandler(ser, command):
-    dataCounter = 0
+#raw data handler
+def handle_data(raw_data, dataCounter):
     TIME = []
     DINPUTS = []
     DOUTPUTS = []
     AINPUTS = []
     AOUTPUTS = []
+    print(raw_data)
+    for i in range(dataCounter):
+        data = raw_data.split('|')
+        TIME[:][i] = data[0].split("\t")
+        DINPUTS[:][i] = data[1].split("\t")
+        DOUTPUTS[:][i] = data[2].split("\t")
+        AINPUTS[:][i] = data[3].split("\t")
+        AOUTPUTS[:][i] = data[4].split("\t")
+    return {TIME, DINPUTS,  DOUTPUTS, AINPUTS, AOUTPUTS}
+
+    # set up thread generators
+def serialHandler(ser, command):
+    dataCounter = 0
     while True:
-        ser.flush()
-        ser.write(command)
-        reading = []
-        while True:
+        rawData = serialPort(ser, command)
+        if dataCounter == 1000:
+            fileWriter(handle_data(rawData, dataCounter))
+            dataCounter = 0
+            break
+        else:
+            dataCounter += 1
+        time.sleep(.2)
+
+
+def serialPort(ser, cmd):
+    if ser.isOpen():
+        try:
+            reading = []
+            ser.flush()
+            ser.write(cmd)
             for c in ser.read():
                 if c == '*':
+                    ser.close()
                     break
                 else:
                     reading.append(c)
             rawData = ''.join(reading)
-            data = rawData.split('|')
-            TIME[:][dataCounter] = data[0].split("\t")
-            DINPUTS[:][dataCounter] = data[1].split("\t")
-            DOUTPUTS[:][dataCounter] = data[2].split("\t")
-            AINPUTS[:][dataCounter] = data[3].split("\t")
-            AOUTPUTS[:][dataCounter] = data[4].split("\t")
-            if dataCounter == 1000:
-                fileWriter(TIME, DINPUTS, DOUTPUTS, AINPUTS, AOUTPUTS)
-                dataCounter = 0
-                break
-            else:
-                dataCounter += 1
-            time.sleep(.2)
+        except:
+            rawData = "Error in serial command"
+            print("Error in serial command")
+    else:
+        rawData = "error in serial command"
+    return rawData
 
 
 
-def serialPort(ser, cmd):
-	
+def DOUT(ser, cmd):
+    rawData = serialPort(ser, cmd)
+    return rawData          # add data handle for this info
 
 
+def AOUT(ser, cmd):
+    rawData = serialPort(ser, cmd)
+    return rawData
 
-def serialCMD(ser, command):
-    ser.flush()
-    ser.write(command)
-    reading = ser.readline()
-    #handle_data(reading, command)
 
+GPIO.add_event_detect(buttonPin, GPIO.FALLING, callback=button_press, bouncetime=300)
 
 while True:
-      main()
+    ser = init_port(port, baud)
+    dataThread = threading.Thread(target=serialHandler, args=(ser, "DATA\r"))
+    dataThread.start()
+    print("test in progress...")
+    time.sleep(5)
